4393fa8901d9b47c3df1f646331592bd9976aabf
Shou Chaofan <shouchaofan@shphschool.com>
2017-08-29 14:22:01 +0800

    MySQL Init

---
+++ MySQLCheck.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+import json, sys, os
+from Logger import Log
+import xml.etree.ElementTree as ET
+from mysql import *
+
+class MySQLCheck(object):
+  pass
+	
+if __name__ == "__main__":
+  pass
