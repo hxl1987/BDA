bc17ed902dad57ea2c8a1a6ef0172a7832ee8c74
Shou C <scf@ieee.org>
2019-03-25 08:08:10 +0800

    Init redis

---
+++ RedisCheck.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from utils import *
+
+
+class RedisCheck(object):
+
+    def __init__(self, conf_path):
+        self.conf_path = conf_path
+
+    def check_exposure(self):
+        pass
+
+    def check_password_setting(self):
+        pass
+
+    def check_config(self):
+        pass
+
+
+def run(conf_path):
+    pass
+
+
+if __name__ == "__main__":
+    pass
